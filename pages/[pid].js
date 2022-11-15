f4993871bd06e2dcbe0a04d046c1bbc17942bbb0
leyuri <leyuri97@gmail.com>
2022-11-15 19:11:18 +0900

    Loading Paths Dynamically

--- pages/[pid].js
+++ pages/[pid].js
@@ -17,16 +17,20 @@ export default function ProductDetailPage(props) {
   );
 }
 
-export async function getStaticProps(context) {
-  const { params } = context;
-
-  const productId = params.pid;
-
+async function getData() {
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  return data;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productId = params.pid;
+  const data = await getData();
   const product = data.products.find((product) => product.id === productId);
+
   return {
     props: {
       loadedProduct: product,
@@ -34,14 +38,13 @@ export async function getStaticProps(context) {
   };
 }
 
-// Generates `/posts/1` and `/posts/2`
 export async function getStaticPaths() {
+  const data = await getData();
+  const ids = data.products.map((product) => product.id);
+  const pathWithParams = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [
-      { params: { pid: "p1" } },
-      // { params: { pid: "p2" } },
-      // { params: { pid: "p3" } },
-    ],
+    paths: pathWithParams,
     fallback: "blocking", // can also be true or 'blocking'
   };
 }
