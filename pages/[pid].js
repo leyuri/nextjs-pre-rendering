c8d0fbda1b8e2358666c67f99a9c190334dc3473
leyuri <leyuri97@gmail.com>
2022-11-15 18:59:17 +0900

    Working With Fallback Pages

--- pages/[pid].js
+++ pages/[pid].js
@@ -2,8 +2,13 @@ import path from "path";
 import fs from "fs/promises";
 import { Fragment } from "react";
 
-export default function ProductDetailPage() {
+export default function ProductDetailPage(props) {
   const { loadedProduct } = props;
+
+  // if (!loadedProduct) {
+  //   return <p>Loading...</p>;
+  // }
+
   return (
     <Fragment>
       <h1>{loadedProduct.title}</h1>
@@ -34,9 +39,9 @@ export async function getStaticPaths() {
   return {
     paths: [
       { params: { pid: "p1" } },
-      { params: { pid: "p2" } },
-      { params: { pid: "p3" } },
+      // { params: { pid: "p2" } },
+      // { params: { pid: "p3" } },
     ],
-    fallback: false, // can also be true or 'blocking'
+    fallback: "blocking", // can also be true or 'blocking'
   };
 }
